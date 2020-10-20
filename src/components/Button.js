6688e7af55d4ff8ec7fd1e5ecf857edcba4d95e4
Ajay David <ajaydavid@ANTONIO-ALGOLIA-MBP.local>
2020-10-19 20:54:03 -0400

    connecting field and button to store

--- src/components/Button.js
+++ src/components/Button.js
@@ -9,7 +9,9 @@ class Button extends React.Component {
         {(color) => (
           <button className={`ui button ${color}`}>
             <LanguageContext.Consumer>
-              {(value) => (value === "english" ? "Submit" : "Voorlegen")}
+              {({ language }) =>
+                language === "english" ? "Submit" : "Voorlegen"
+              }
             </LanguageContext.Consumer>
           </button>
         )}
